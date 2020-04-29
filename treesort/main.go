6469dd027d662eb8e8ca85d8fe7f544f0fd39709
Aditya Chempakasseril <adityac@live.com>
2020-04-29 15:04:28 -0700

    tree sort

---
+++ treesort/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+// Uses a binary tree to implement an insertion sort
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues appends the elements of t to values in order
+// and returns the resulting slice
+// This is done with in-order traversal
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
+
+func main() {
+	test := []int{6, 2, 4, 7, 91, 2, 4, 435, 567, 0}
+	fmt.Println(test)
+	Sort(test)
+	fmt.Println(test)
+}
